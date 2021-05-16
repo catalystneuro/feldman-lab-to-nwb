b10b83e6ffec59fea3f769b8c9777de5aa01d3f7
Cody Baker <cbaker9@nd.edu>
2021-05-16 14:14:04 -0400

    added ap electrode metadata to conversion for new widget

--- feldman_lab_to_nwb/convert_feldman/rapidtestingdatainterface.py
+++ feldman_lab_to_nwb/convert_feldman/rapidtestingdatainterface.py
@@ -1,12 +1,27 @@
 """Authors: Cody Baker."""
+from pathlib import Path
+
 from nwb_conversion_tools import SpikeGLXRecordingInterface
 from pynwb import NWBFile
+from spikeextractors import NwbRecordingExtractor, SpikeGLXRecordingExtractor
 
 from .feldman_utils import get_trials_info
 
 
+def infer_ap_filepath(nidq_file_path: str):
+    session_name = Path(nidq_file_path).parent.stem
+    ap_filepath = nidq_file_path.replace("_t0.nidq.bin", f"_imec0/{session_name}_t0.imec0.ap.bin")
+    return ap_filepath
+
+
 class RapidTestingDataInterface(SpikeGLXRecordingInterface):
     """Conversion class for the Feldman lab behavioral data."""
+
+    def get_metadata(self):
+        electrode_interface = SpikeGLXRecordingInterface(
+            file_path=infer_ap_filepath(nidq_file_path=self.source_data["file_path"])
+        )
+        return electrode_interface.get_metadata()
 
     def run_conversion(self, nwbfile: NWBFile, metadata: dict):
         """
@@ -14,6 +29,13 @@ class RapidTestingDataInterface(SpikeGLXRecordingInterface):
 
         Does not require the more detailed behavioral csv files.
         """
+        electrode_extractor = SpikeGLXRecordingExtractor(
+            file_path=infer_ap_filepath(nidq_file_path=self.source_data["file_path"])
+        )
+        NwbRecordingExtractor.add_devices(recording=electrode_extractor, nwbfile=nwbfile, metadata=metadata)
+        NwbRecordingExtractor.add_electrode_groups(recording=electrode_extractor, nwbfile=nwbfile, metadata=metadata)
+        NwbRecordingExtractor.add_electrodes(recording=electrode_extractor, nwbfile=nwbfile, metadata=metadata)
+
         (trial_numbers, stimulus_numbers, segment_numbers_from_nidq, trial_times_from_nidq) = get_trials_info(
             recording_nidq=self.recording_extractor
         )
