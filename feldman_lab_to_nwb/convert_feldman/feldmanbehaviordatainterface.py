36189f4b5fe0135bd659790aa751e91ff7383347
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2021-07-02 14:50:54 -0400

    cleaning for PR

--- feldman_lab_to_nwb/convert_feldman/feldmanbehaviordatainterface.py
+++ feldman_lab_to_nwb/convert_feldman/feldmanbehaviordatainterface.py
@@ -5,7 +5,9 @@ from warnings import warn
 import re
 import numpy as np
 
+from ndx_events import AnnotatedEventsTable
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
+from nwb_conversion_tools.conversion_tools import get_module
 from pynwb import NWBFile
 from spikeextractors import SpikeGLXRecordingExtractor
 
@@ -18,9 +20,9 @@ class FeldmanBehaviorDataInterface(BaseDataInterface):
     @classmethod
     def get_source_schema(cls):
         return dict(
-            required=["folder_path"],
+            required=['folder_path'],
             properties=dict(
-                folder_path=dict(type="string")
+                folder_path=dict(type='string')
             )
         )
 
@@ -48,35 +50,15 @@ class FeldmanBehaviorDataInterface(BaseDataInterface):
 
         stim_layout_str = ["Std", "Trains", "IL", "Trains+IL", "RFMap", "2WC", "MWS", "MWD"]
 
-        nwbfile.add_trial_column(
-            name="stimulus_time",
-            description="The timestamp of the stimulus presentation within the trial."
-        )
-        nwbfile.add_trial_column(name="stimulus_number", description="The identifier value for stimulus type.")
-        nwbfile.add_trial_column(name="stimulus_name", description="The custom label for the stimulus number.")
-        nwbfile.add_trial_column(name="trial_type", description="The type index for each trial.")
-        nwbfile.add_trial_column(name="trial_outcome", description="The outcome index for each trial.")
-        # nwbfile.add_trial_column(
-        #     name="ordinality",
-        #     description="Ordinal position of this element within the trial. E.g., '1' is the first stimulus in trial."
-        # )
-        nwbfile.add_trial_column(name="amplitude", description="Amplitude of stimulus in microns.")
-        nwbfile.add_trial_column(name="probability", description="Probability of stimulus being presented.")  # check descr
-        nwbfile.add_trial_column(name="duration", description="Duration of stimulus in seconds.")
-        nwbfile.add_trial_column(name="shape", description="Shape index of stimulus.")  # check descr
-        nwbfile.add_trial_column(name="rise", description="Rise index of stimulus.")  # check descr
-        nwbfile.add_trial_column(name="gng", description="GNG of stimulus.")  # check descr??
+        elements = []
+        element_times = []
+        amplitude = []
+        ordinality = []
+        element_index_label_pairs = []
+        trial_starts = []
+        trial_ends = []
         for header_segment in header_segments:
             header_data = pd.read_csv(header_segment, header=None, sep="\t", index_col=0).T
-            trial_data = pd.read_csv(str(header_segment).replace("header", "trials"), header=0, sep="\t")
-            # only u sed for ordinality
-            # stimuli_data = pd.read_csv(str(header_segment).replace("header", "stimuli"), header=0, sep="\t")
-
-            stim_layout = int(header_data["StimLayout"].values[0]) - 1  # -1 for zero-indexing
-            if stim_layout != 0:
-                raise NotImplementedError(
-                    f"StimLayouts other than '{stim_layout_str[stim_layout]}' type have not yet been implemented!"
-                )
 
             segment_number = header_data["SegmentNum"]
             p = re.compile("_S(\d+)_")
@@ -88,51 +70,82 @@ class FeldmanBehaviorDataInterface(BaseDataInterface):
                 )
             segment_number_from_file_name = int(res.group(1))
 
-            n_stim = int(header_data["StdStimN"])
-            if n_stim != 1:
+            stim_layout = int(header_data["StimLayout"].values[0]) - 1  # -1 for zero-indexing
+            if stim_layout != 0:
                 raise NotImplementedError(
-                    f"StdStimN ({n_stim}) from header _data in segment file {header_segment} is not yet supported!"
+                    f"StimLayouts other than '{stim_layout_str[stim_layout]}' type have not yet been implemented!"
                 )
+
+            trial_data = pd.read_csv(str(header_segment).replace("header", "trials"), header=0, sep="\t")
+            trial_starts.extend(trial_data['TrStartTime'] / 1e3)
+            trial_ends.extend(trial_data['TrEndTime'] / 1e3)
+            # may have to correct these by ISS0 shifts? And/or TTL pulses?
+
+            stimuli_data = pd.read_csv(str(header_segment).replace("header", "stimuli"), header=0, sep="\t")
+            elements.extend(trial_data["StimNum"].tolist())
+
+            # use the nidq time instead of csv time
             segment_trial_starts = trial_times_from_nidq[segment_numbers_from_nidq == segment_number_from_file_name, 0]
-            segment_trial_ends = trial_times_from_nidq[segment_numbers_from_nidq == segment_number_from_file_name, 1]
-            segment_stimulus_onset_time = int(header_data["StdStimOnset"]) / 1e3
-            stimulus_times = segment_trial_starts + segment_stimulus_onset_time
+            element_times.extend(segment_trial_starts + stimuli_data["Time_ms"] / 1e3)
 
-            # Ordinality would be used in extending n_stim > 1
-            # ordinality = stimuli_data["Posn"]
-
-            amplitude_map = [int(x) for x in header_data[[x for x in header_data if "ElemAmp" in x]].iloc[0]]
-            probability_map = [int(x) for x in header_data[[x for x in header_data if "ElemProb" in x]].iloc[0]]
-            duration_map = [int(x) / 1e3 for x in header_data[[x for x in header_data if "ElemDur" in x]].iloc[0]]
-            shape_map = [int(x) for x in header_data[[x for x in header_data if "ElemShape" in x]].iloc[0]]
-            rise_map = [int(x) for x in header_data[[x for x in header_data if "ElemRise" in x]].iloc[0]]
-            GNG_map = [int(x) for x in header_data[[x for x in header_data if "ElemGNG" in x]].iloc[0]]
+            amplitude.extend(stimuli_data["Ampl"])
+            ordinality.extend(stimuli_data["Posn"])
 
             elem_piezo = {x: header_data[x].values[0] for x in header_data if "ElemPiezo" in x}
             elem_piezo_labels = {x: header_data[x].values[0] for x in header_data if "PiezoLabel" in x}
             assert len(elem_piezo) == len(elem_piezo_labels), "Size mismatch between element piezo and piezo labels!"
-            element_index_label_pairs = [
-                [elem_piezo[x], elem_piezo_labels[y]] for x, y in zip(elem_piezo, elem_piezo_labels)
-            ]
-            element_index_label_map = dict(np.unique(element_index_label_pairs, axis=0))
+            element_index_label_pairs.extend(
+                [[elem_piezo[x], elem_piezo_labels[y]] for x, y in zip(elem_piezo, elem_piezo_labels)]
+            )
+        assert len(trial_numbers) == len(trial_starts), \
+            "Mismatch between number of trials extracted from nidq file and number reported in trials.csv file!"
+        assert (stimulus_numbers == elements).all(), \
+            "Mismatch between stimulus numbers extracted from nidq file and those reported in trials.csv file!"
 
-            trial_types = list(trial_data["TrType"])
-            trial_outcomes = list(trial_data["TrOutcome"])
+        element_index_label_map = dict(np.unique(element_index_label_pairs, axis=0))
+        annotated_events = AnnotatedEventsTable(
+            name="StimulusEvents",
+            description="Stimulus elements, events, and properties.",
+            resolution=np.nan
+        )
+        annotated_events.add_column(
+            name="amplitude",
+            description="Amplitude (microns) of element.",
+            index=True
+        )
+        annotated_events.add_column(
+            name="ordinality",
+            description="Ordinal position of this element within the trial. E.g., '1' is the first stimulus in trial.",
+            index=True
+        )
+        annotated_events.add_column(
+            name="element_id",
+            description="The assigned element ID.",
+            index=True
+        )
+        annotated_events.add_column(
+            name="element_name",
+            description="The custom label for the element ID.",
+            index=True
+        )
+        # event_times can't be compressed, returned error
+        # Tried adding compression to other columns, and it took a H5DataIO type, but didn't actually perform it
+        annotated_events.add_event_type(
+            label="Element",
+            event_description="Time (in seconds) when an element was presented.",
+            event_times=element_times,
+            amplitude=amplitude,
+            ordinality=ordinality,
+            element_id=elements,
+            element_name=[element_index_label_map[str(x)] for x in elements]
+        )
+        get_module(nwbfile=nwbfile, name="events", description="Processed event data.").add(annotated_events)
 
-            for k in range(len(segment_trial_starts)):
-                nwbfile.add_trial(
-                    start_time=segment_trial_starts[k],
-                    stop_time=segment_trial_ends[k],
-                    stimulus_time=stimulus_times[k],
-                    stimulus_number=stimulus_numbers[k],
-                    stimulus_name=element_index_label_map[str(stimulus_numbers[k])],
-                    trial_type=trial_types[k],
-                    trial_outcome=trial_outcomes[k],
-                    # ordinality=ordinality[k],
-                    amplitude=amplitude_map[stimulus_numbers[k]],
-                    probability=probability_map[stimulus_numbers[k]],
-                    duration=duration_map[stimulus_numbers[k]],
-                    shape=shape_map[stimulus_numbers[k]],
-                    rise=rise_map[stimulus_numbers[k]],
-                    gng=GNG_map[stimulus_numbers[k]]
-                )
+        # use trial times from nidq file
+        nwbfile.add_trial_column(name="stim_number", description="The identifier value for stimulus type.")
+        for k in range(len(trial_times_from_nidq)):
+            nwbfile.add_trial(
+                start_time=trial_times_from_nidq[k, 0],
+                stop_time=trial_times_from_nidq[k, 1],
+                stim_number=stimulus_numbers[k]
+            )
